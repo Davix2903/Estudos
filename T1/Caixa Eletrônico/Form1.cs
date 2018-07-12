33cfe1d99eeb341df61022d5d549db4912873f84
v012 <v012@BNU-ESTAG012>
2018-07-12 16:55:14 -0300

    Projeto de caixa eletrônico.

---
+++ T1/Caixa Eletrônico/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caixa_Eletrônico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double valor = 1000.0;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+        }
+private void button1_Click_2(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double valor = 10.0;
+
+            if (valor <= saldo) ;
+            saldo = saldo - valor;
+            MessageBox.Show("Saque realizado com sucesso!");
+            MessageBox.Show("Saldo atual: " + saldo);
+
+        }   else 
+    }
+    }
+
