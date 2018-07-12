2d14735f2bdfdd6a80c051560ef6a4bbcc72c1fd
v012 <v012@BNU-ESTAG012>
2018-07-12 16:58:44 -0300

    Média das idades

---
+++ T1/Média das Idades/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Média_das_Idades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Wanessa = 22;
+            int Wagner = 18;
+            int Lucas = 14;
+
+            double MedindoIdades = (Wagner + Wanessa + Lucas) / 3;
+
+            MessageBox.Show("A média das idades é: " + MedindoIdades);
+        }
+    }
+}
