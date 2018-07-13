008bdb696b6d54dd01a7b02f326f59678e226fe6
v012 <v012@BNU-ESTAG012>
2018-07-13 16:50:32 -0300

    Número divisíveis por 3 e 4 (até 30)

---
+++ T1/Mão na Massa/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mão_na_Massa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            for (i = 1; i <= 30; i = i + 1)
+            {
+                if (i % 3 == 0)
+                    MessageBox.Show("O número " + i + " é dívisvel por 3");
+                if (i % 4 == 0)
+                {
+                    MessageBox.Show("O número " + i + " é divisível por 4");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 30; i = i+1)
+            {
+                int NúmeroUm = 3;
+                int NúmeroDois = 4;
+                if (i % NúmeroUm == 0)
+                {
+                    MessageBox.Show("O número " + i + " é divisível por: " +NúmeroUm );
+                }
+                if (i % NúmeroDois == 0)
+                    {
+                        MessageBox.Show("O numero " + i + " é divisível por: " + NúmeroDois) ;
+                    }
+                }
+            }
+        }
+    }
