4786c3f549934826c20ae4c47c1c35aee28af909
v012 <v012@BNU-ESTAG012>
2018-07-16 14:20:03 -0300

    utilidade caixa eletrônico

---
+++ T1/Utilidades caixa eletrônico/Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades_caixa_eletrônico
+{
+    class Conta
+    {
+        public int numero;
+
+        public string titular;
+
+        public double saldo;
+    }
+}
---
+++ T1/Utilidades caixa eletrônico/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Utilidades_caixa_eletrônico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroDaConta1 = 1;
+            string titularDaConta1 = "James Howlett";
+            double saldoDaConta1 = 2000.0;
+
+            int numeroDaConta2 = 2;
+            string titularDaConta2 = "Charles Xavier";
+            double saldoDaConta2 = 5000.0;
+        }
+    }
+}
