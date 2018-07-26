1fa57ba88542f85f13b027bb4ecfd1756b045349
v012 <v012@BNU-ESTAG012>
2018-07-26 17:28:07 -0300

    Entity

--- T1/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
@@ -11,7 +12,52 @@ namespace Alura.Loja.Testes.ConsoleApp
         static void Main(string[] args)
         {
             //GravarUsandoAdoNet();
+            //GravarUsandoEntity();
+            //RecuperarProdutos();
+            //ExcluirProdutos();
+            //RecuperarProdutos();
+            AtualizarProdutos();
+        }
+
+        private static void AtualizarProdutos()
+        {
+            //Incluir um produto
             GravarUsandoEntity();
+            RecuperarProdutos();
+
+            //Atualizar o produto
+            using (var repo = new ProdutoDAOEntity())
+            {
+                Produto primeiro = repo.Produtos().First();
+                primeiro.Nome = "Harry Potter - editado";
+                repo.Atualizar(primeiro);
+            }
+            RecuperarProdutos();
+        }
+
+        private static void ExcluirProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+                foreach (var item in produtos)
+                {
+                    repo.Remover(item);
+                }
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+                Console.WriteLine("Foram encontrados {0} produtos(s).", produtos.Count);
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
         }
 
         private static void GravarUsandoEntity()
@@ -21,9 +67,9 @@ namespace Alura.Loja.Testes.ConsoleApp
             p.Categoria = "Livros";
             p.Preco = 19.89;
 
-            using (var contexto = new LojaContext())
+            using (var contexto = new ProdutoDAOEntity())
             {
-                repo.Adicionar(p);
+                contexto.Adicionar(p);
             }
         }
 
