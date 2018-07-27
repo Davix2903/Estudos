b9b433a8a2c209f071a940d165da222461a3f3aa
v012 <v012@BNU-ESTAG012>
2018-07-27 16:49:09 -0300

    Removendo ADO .Net

--- T1/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,42 +11,8 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var produtos = contexto.Produtos.ToList();
 
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //var novoProduto = new Produto()
-                //{
-                //    Nome = "Desinfetante",
-                //    Categoria = "Limpeza",
-                //    Preco = 2.99
-                //};
-                //contexto.Produtos.Add(novoProduto);
-
-                var p1 = produtos.First();
-                contexto.Produtos.Remove(p1);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-
-                //ExibeEntries(contexto.ChangeTracker.Entries());
-            }
-        }
-
-
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            foreach (var e in entries)
-            {
-                Console.WriteLine("==============");
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
-
-        }
+        }        
     }
 }
 
