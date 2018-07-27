7a2a568bdb4ef534c6e09fcfd5721022bab3f34f
v012 <v012@BNU-ESTAG012>
2018-07-27 17:27:18 -0300

    Migrations

--- T1/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ T1/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -6,10 +6,11 @@
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
         public double PrecoUnitario { get; internal set; }
+        public string Unidade { get; internal set; }
 
         public override string ToString()
         {
-            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.Preco} + {this.Nome} ";
+            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.PrecoUnitario} + {this.Nome} ";
         }
     }
 }
