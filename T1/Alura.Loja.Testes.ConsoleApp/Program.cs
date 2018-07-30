d8f144f0b72e35700db0558b79fe78ded7ff24e2
v012 <v012@BNU-ESTAG012>
2018-07-30 17:00:08 -0300

    Promoção

--- T1/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,29 +1,22 @@
-﻿namespace Alura.Loja.Testes.ConsoleApp
+﻿using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            //compra de 6 pães franceses
-            var PaoFrances = new Produto();
-            PaoFrances.Nome = "Pão Francês";
-            PaoFrances.PrecoUnitario = 0.40;
-            PaoFrances.Categoria = "Padaria";
+            var promocaoDePascoa = new Promocao();
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
+            promocaoDePascoa.Produtos.Add(new Produto());
 
-            var compra = new Compra();
-            compra.Quantidade = 6;
-            compra.PrecoTotal = PaoFrances.PrecoUnitario * compra.Quantidade;
-            compra.Produto = PaoFrances;
-
-            using(var contexto = new LojaContext())
-        {
-              
-
-                contexto.Compras.Add(compra);
-
+            using (var contexto = new LojaContext())
+            {
                 contexto.SaveChanges();
             }
-        }        
+        }
+
     }
 }
-
---
+++ T1/Alura.Loja.Testes.ConsoleApp/Promocao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Promocao
+    {
+        public int Id { get; set; }
+        public object DataInicio { get; internal set; }
+        public DateTime DataTermino { get; internal set; }
+        public IList<PromocaoProduto> Produtos { get; internal set; }
+    }
+}
