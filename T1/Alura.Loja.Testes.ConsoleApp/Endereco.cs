d53515c088abdc8b3644ec3aa69f48566d9abc99
v012 <v012@BNU-ESTAG012>
2018-07-31 15:02:54 -0300

    Relacionamentos

---
+++ T1/Alura.Loja.Testes.ConsoleApp/Endereco.cs
@@ -0,0 +1,12 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Endereco
+    {
+        public int EnderecoId { get; set; }
+        public string Numero { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
--- T1/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,22 +1,48 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            var promocaoDePascoa = new Promocao();
-            promocaoDePascoa.DataInicio = DateTime.Now;
-            promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
-            promocaoDePascoa.Produtos.Add(new Produto());
+            var fulano = new Cliente();
+            fulano.Nome = "Fulaninho de Tal";
+            fulano.EnderecoDeEntrega = new Endereco()
+            {
+                Numero = "100",
+                Rua = "Rua Mexicana",
+                Bairro = "Ruínas",
+                Cidade = "São Paulo"
+            };
 
             using (var contexto = new LojaContext())
             {
-                contexto.SaveChanges();
-            }
-        }
 
+            }
+
+            //static void Main(string[] args)
+            //{
+            //    //var promocaoDePascoa = new Promocao();
+            //    //promocaoDePascoa.DataInicio = DateTime.Now;
+            //    //promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
+            //    ////promocaoDePascoa.Produtos.Add(new Produto());
+
+            //    ////using (var contexto = new LojaContext())
+            //    ////{
+            //    ////    //contexto.SaveChanges();
+            //    ////}
+            //}
+
+        }
     }
 }
