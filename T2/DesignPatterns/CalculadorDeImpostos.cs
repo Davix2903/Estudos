00355849819874019a081c0d502c139c61ba920e
v012 <v012@benner.com.br>
2018-08-14 17:22:12 -0300

    Design Patterns

---
+++ T2/DesignPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCalculo(Orcamento orcamento)
+        {
+            if ("ICMS".Equals(imposto))
+            {
+                double icms = orcamento.Valor * 0.1;
+                Console.WriteLine(icms);
+            }
+            else if (true)
+            {
+                double iss = orcamento.Valor * 0.06;
+                Console.WriteLine(iss);
+            }
+
+
+        }
+
+    }
+}
