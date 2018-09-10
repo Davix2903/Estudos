48b1629ce2a9a3b862d2d0a28e2055b8f40df216
Davi Henrique <v012@benner.com.br>
2018-09-10 14:00:31 -0300

    adicionando metodos - c# reflection

---
+++ T2/ByteBank Portal/infraestrutura/Utilidades.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_Portal.infraestrutura
+{
+    public static class Utilidades
+    {
+
+        public static string ConverterPathParaNomeAssembly(string path)
+        {
+
+            var prefixoAssembly = "ByteBank.Portal";
+            var pathComPontos = path.Replace('/', '.');
+
+            var nomeCompleto = $"{prefixoAssembly}.{pathComPontos}";
+
+            return nomeCompleto;
+        }
+
+        public static string ObterTipoDeConteudo(string path)
+        {
+            if (path.EndsWith(".css"))
+            {
+                return "content"
+            }
+        }
+
+    }
+}
