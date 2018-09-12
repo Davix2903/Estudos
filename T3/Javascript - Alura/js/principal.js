8a24583d54dcbf37f0073707ce970a9a6ce8707b
Davi Henrique <v012@benner.com.br>
2018-09-12 16:40:45 -0300

    && - ||

--- T3/Javascript - Alura/js/principal.js
+++ T3/Javascript - Alura/js/principal.js
@@ -9,7 +9,29 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-console.log(paciente);
-console.log(tdPeso);
-console.log(peso);
-console.log(altura);
+var tdImc = paciente.querySelector(".info-imc");
+
+var PesoValido = true;
+var AlturaValida = true;
+
+//Validação de Peso:
+if (peso > 0 && peso < 500) {
+    PesoValido = true;
+} else if(peso <= 0 || peso > 500) {
+    PesoValido = false;
+    tdImc.textContent = "Peso Inválido!";
+}
+
+//Validação de Altura:
+if (altura > 0 && altura < 3.00) {
+    AlturaValida = true;
+} else if(altura <= 0 || altura > 3.00) {
+    AlturaValida = false;
+    tdImc.textContent = "Altura Inválida!";
+}
+
+//Validação Final:
+if (AlturaValida && PesoValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
